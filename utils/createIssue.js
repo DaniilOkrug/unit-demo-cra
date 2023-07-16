5bbb3c93cecaaf1ae8b6cb40c0bee14522bfbed0
Daniil <daniil.okrug@gmail.com>
2023-07-16 07:36:08 +0300

    ci(release): changed release pipline

--- utils/createIssue.js
+++ utils/createIssue.js
@@ -1,46 +1,48 @@
-const { Octokit } = require("octokit");
 const core = require('@actions/core');
 const github = require('@actions/github');
 
 const releaseTag = core.getInput('ref');
 const authToken = core.getInput('token');
 const repository = core.getInput('repo');
+const owner = core.getInput('owner');
 
 console.log('releaseTag', releaseTag);
 console.log('repository', repository);
+console.log('owner', owner);
+console.log('authToken', authToken);
 
-const octokit = new Octokit({ authToken })
+// const octokit = new Octokit({ authToken })
 
-const owner = core.getInput('owner');
-const time = (new Date()).toTimeString();
+// const owner = core.getInput('owner');
+// const time = (new Date()).toTimeString();
 
-const REPO_INFO = {
-  owner,
-  repo: "unit-demo-cra",
-}
+// const REPO_INFO = {
+//   owner,
+//   repo: "unit-demo-cra",
+// }
 
-// Get all issues
-async function getAllIssues() {
-  return await octokit.paginate(octokit.rest.issues.listForRepo, {
-    ...REPO_INFO, per_page: 100,
-  })
-}
+// // Get all issues
+// async function getAllIssues() {
+//   return await octokit.paginate(octokit.rest.issues.listForRepo, {
+//     ...REPO_INFO, per_page: 100,
+//   })
+// }
 
-try {
-  getAllIssues((issues) => {
-    console.log('issues', issues);
+// try {
+//   getAllIssues((issues) => {
+//     console.log('issues', issues);
 
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+//     const payload = JSON.stringify(github.context.payload, undefined, 2)
+//     console.log(`The event payload: ${payload}`);
 
-    // octokit.rest.issues.create({
-    //   ...REPO_INFO,
-    //   title: 'Relesase',
-    //   body: 'Release'
-    // });
-  })
+//     // octokit.rest.issues.create({
+//     //   ...REPO_INFO,
+//     //   title: 'Relesase',
+//     //   body: 'Release'
+//     // });
+//   })
 
 
-} catch (error) {
-  core.setFailed(error.message);
-}
+// } catch (error) {
+//   core.setFailed(error.message);
+// }
